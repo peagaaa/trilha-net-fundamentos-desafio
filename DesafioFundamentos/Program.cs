dbe7ab78550a667b8416199270ace292a9798c1d
Peu <peixoto05henrique@gmail.com>
2024-04-27 14:25:04 -0300

    Project_cloncluido

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,13 +1,14 @@
-﻿using DesafioFundamentos.Models;
+﻿// Caminho lógico da minha classe estacionamento.
+using DesafioFundamentos.Models;
 
-// Coloca o encoding para UTF8 para exibir acentuação
+// Coloca o encoding para UTF8 para exibir acentuação.
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento Hillow!\n" +
+                    "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
